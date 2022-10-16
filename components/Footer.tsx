57e7e3d997b3e00357732da6237ee016b5df3fe7
acapodanno <alessandro.capodanno95@gmail.com>
2022-10-16 08:25:35 +0200

    add

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,12 @@
 // components/footer.tsx
 import React from 'react';
 
-export const Footer: React.FC = () => {
+const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 right-0 text-2xl m-2">
       Create by Fabio Biondi & Adriano Grimaldi
     </footer>
   )
 }
+
+export default Footer
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Footer } from "../components/Footer"
-import  Navbar  from "../components/Navbar"
-
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <div>
-    <Navbar />
-    <div className="page">
-      <Component {...pageProps} />
-    </div>
-    <Footer />
-  </div>)
+    <div>
+      <Navbar />
+      <div className="page">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>)
 
 }
 
